af509decfe8929f67398bf8db48ebf24ab27c938
marcusfaust <marcus.faust1@gmail.com>
2014-09-16 12:46:52 -0700

    modified:   app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,4 +11,4 @@ def hello_world():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug = True)
+    app.run(host='0.0.0.0', port=port, debug=True)
