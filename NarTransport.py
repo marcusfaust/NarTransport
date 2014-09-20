f5ddccde00b9177ce04183f3321e4bcf1ef1d9b4
marcusfaust <marcus.faust1@gmail.com>
2014-09-20 01:07:13 -0700

    modified:   NarTransport.py

--- NarTransport.py
+++ NarTransport.py
@@ -187,7 +187,7 @@ if __name__ == '__main__':
 
                 #Append Datetime to Folder Name
                 folderWithDate = folder['name'] + "_" + datetime.now().isoformat()
-                boxsession.renameFolder(folder['id'],folderWithDate, access_token)
+                boxsession.renameFolder(folder['id'], folderWithDate, access_token)
 
                 #Move Folder to Archive Folder
                 boxsession.moveFolder(folder['id'], box_archive_folder_id, access_token)
