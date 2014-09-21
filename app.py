e33319ef5645c0641135e4d15822089acdf0317b
marcusfaust <marcus.faust1@gmail.com>
2014-09-21 03:59:14 -0700

    modified:   app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
-import models
+
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+import models
 
 @app.route('/')
 def hello_world():
