0012ea007195aa22962d0b7fa2bcf96e5b2077b9
marcusfaust <marcus.faust1@gmail.com>
2014-09-20 17:33:04 -0700

    modified:   models.py 	new file:   templates/404.html 	new file:   templates/base.html 	new file:   templates/index.html 	new file:   templates/output.html 	new file:   templates/upload.html

--- models.py
+++ models.py
@@ -10,3 +10,14 @@ class RefreshToken(db.Model):
 
     def __repr__(self):
         return '<token {}>'.format(self.token)
+
+class RunLog(db.Model):
+    __tablename__ = 'runlog'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    datetime = db.Column(db.DateTime())
+    nars_found = db.Column(db.Boolean())
+    duration = db.Column(db.Integer())
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
